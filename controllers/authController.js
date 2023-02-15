980901bd4378f5edb6f1a2a57f03655e801321b0
Ari Rahim <aritomsk@yahoo.com>
2023-02-15 21:56:49 +0300

    fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
-const User = require("../models/userModel");
+// const User = require("../models/userModel");
+import User from "../models/userModel";
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { config } = require("../config");
