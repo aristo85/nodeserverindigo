49b943490d1182069a75340b78c3523d5d6e6f89
Ari Rahim <aritomsk@yahoo.com>
2023-02-15 17:14:39 +0300

    add vercel

--- utils/validation/signupValidation.js
+++ utils/validation/signupValidation.js
@@ -6,8 +6,8 @@ exports.singupValidationSchema = yup.object({
         email: yup.string().email('Must be a valid email')
             .max(255).required('Email is required'),
         password: yup.string().min(5).max(255).required('Password is required'),
-        birthdate: yup.date().required('Birth date is required'),
-        gender: yup.string().oneOf(["male", "female"]).required(),
+        // birthdate: yup.date().required('Birth date is required'),
+        // gender: yup.string().oneOf(["male", "female"]).required(),
     }),
     // params: yup.object({
     //     id: yup.number().required(),
